2ebd72a75605ef5d2731e4bf66cf298087a5fd87
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-16 14:44:28 +0200

    Task_1

---
+++ SQL_more_queries/1-create_user.sql
@@ -0,0 +1,32 @@
+/* 
+1. Root user
+mandatory
+
+Write a script that creates the MySQL server user user_0d_1.
+
+    user_0d_1 should have all privileges on your MySQL server
+    The user_0d_1 password should be set to user_0d_1_pwd
+    If the user user_0d_1 already exists, your script should not fail
+
+guillaume@ubuntu:~/$ cat 1-create_user.sql | mysql -hlocalhost -uroot -p
+Enter password: 
+guillaume@ubuntu:~/$ cat 0-privileges.sql | mysql -hlocalhost -uroot -p
+Enter password: 
+Grants for user_0d_1@localhost
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost'
+ERROR 1141 (42000) at line 4: There is no such grant defined for user 'user_0d_2' on host 'localhost'
+guillaume@ubuntu:~/$ 
+
+Repo:
+
+    GitHub repository: alx_database
+    Directory: SQL_more_queries
+    File: 1-create_user.sql
+
+ */
+
+-- creates the MySQL server user user_0d_1
+-- creates a new user
+CREATE USER IF NOT EXISTS user_0d_1@localhost IDENTIFIED BY 'user_0d_1_pwd';
+-- grants privileges
+GRANT ALL PRIVILEGES ON * . * TO user_0d_1@localhost;
