a494c6c3e70c741bc8ace20ee4492249c6a4135e
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-16 15:55:35 +0200

    Task_6

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,34 +1,33 @@
-
--- 3. Always a name
+-- 6. States table
 -- mandatory
 
--- Write a script that creates the table force_name on your MySQL server.
+-- Write a script that creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server.
 
---     force_name description:
---         id INT
+--     states description:
+--         id INT unique, auto generated, can’t be null and is a primary key
 --         name VARCHAR(256) can’t be null
---     The database name will be passed as an argument of the mysql command
---     If the table force_name already exists, your script should not fail
+--     If the database hbtn_0d_usa already exists, your script should not fail
+--     If the table states already exists, your script should not fail
 
--- guillaume@ubuntu:~/$ cat 3-force_name.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- guillaume@ubuntu:~/$ cat 6-states.sql | mysql -hlocalhost -uroot -p
 -- Enter password: 
--- guillaume@ubuntu:~/$ echo 'INSERT INTO force_name (id, name) VALUES (89, "Holberton School");' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- guillaume@ubuntu:~/$ echo 'INSERT INTO states (name) VALUES ("California"), ("Arizona"), ("Texas");' | mysql -hlocalhost -uroot -p hbtn_0d_usa
 -- Enter password: 
--- guillaume@ubuntu:~/$ echo 'SELECT * FROM force_name;' | mysql -hlocalhost -uroot -p hbtn_0d_2
--- Enter password: 
--- id  name
--- 89  Holberton School
--- guillaume@ubuntu:~/$ echo 'INSERT INTO force_name (id) VALUES (333);' | mysql -hlocalhost -uroot -p hbtn_0d_2
--- Enter password: 
--- ERROR 1364 (HY000) at line 1: Field 'name' doesn't have a default value
--- guillaume@ubuntu:~/$ echo 'SELECT * FROM force_name;' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- guillaume@ubuntu:~/$ echo 'SELECT * FROM states;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
 -- Enter password: 
 -- id  name
--- 89  Holberton School
+-- 1   California
+-- 2   Arizona
+-- 3   Texas
 -- guillaume@ubuntu:~/$ 
 
 -- //////////////////////////////////////////////////////////////////////////////
 
--- creates the table force_name on your MySQL server
--- creates a table in a database
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+-- creates the database hbtn_0d_usa and the table states (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL, PRIMARY KEY(id));
+
