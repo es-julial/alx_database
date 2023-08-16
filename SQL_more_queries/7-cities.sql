76b196821d05dc8979fccfdb866a35b4bcd8dc68
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-16 15:56:57 +0200

    Task_7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -1,34 +1,43 @@
 
--- 3. Always a name
+-- 7. Cities table
 -- mandatory
 
--- Write a script that creates the table force_name on your MySQL server.
+-- Write a script that creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server.
 
---     force_name description:
---         id INT
+--     cities description:
+--         id INT unique, auto generated, can’t be null and is a primary key
+--         state_id INT, can’t be null and must be a FOREIGN KEY that references to id of the states table
 --         name VARCHAR(256) can’t be null
---     The database name will be passed as an argument of the mysql command
---     If the table force_name already exists, your script should not fail
+--     If the database hbtn_0d_usa already exists, your script should not fail
+--     If the table cities already exists, your script should not fail
 
--- guillaume@ubuntu:~/$ cat 3-force_name.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- guillaume@ubuntu:~/$ cat 7-cities.sql | mysql -hlocalhost -uroot -p
 -- Enter password: 
--- guillaume@ubuntu:~/$ echo 'INSERT INTO force_name (id, name) VALUES (89, "Holberton School");' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- guillaume@ubuntu:~/$ echo 'INSERT INTO cities (state_id, name) VALUES (1, "San Francisco");' | mysql -hlocalhost -uroot -p hbtn_0d_usa
 -- Enter password: 
--- guillaume@ubuntu:~/$ echo 'SELECT * FROM force_name;' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- guillaume@ubuntu:~/$ echo 'SELECT * FROM cities;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
 -- Enter password: 
--- id  name
--- 89  Holberton School
--- guillaume@ubuntu:~/$ echo 'INSERT INTO force_name (id) VALUES (333);' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- id  state_id    name
+-- 1   1   San Francisco
+-- guillaume@ubuntu:~/$ echo 'INSERT INTO cities (state_id, name) VALUES (10, "Paris");' | mysql -hlocalhost -uroot -p hbtn_0d_usa
 -- Enter password: 
--- ERROR 1364 (HY000) at line 1: Field 'name' doesn't have a default value
--- guillaume@ubuntu:~/$ echo 'SELECT * FROM force_name;' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- ERROR 1452 (23000) at line 1: Cannot add or update a child row: a foreign key constraint fails (`hbtn_0d_usa`.`cities`, CONSTRAINT `cities_ibfk_1` FOREIGN KEY (`state_id`) REFERENCES `states` (`id`))
+-- guillaume@ubuntu:~/$ echo 'SELECT * FROM cities;' | mysql -hlocalhost -uroot -p hbtn_0d_usa
 -- Enter password: 
--- id  name
--- 89  Holberton School
+-- id  state_id    name
+-- 1   1   San Francisco
 -- guillaume@ubuntu:~/$ 
-
+ 
 -- //////////////////////////////////////////////////////////////////////////////
 
--- creates the table force_name on your MySQL server
--- creates a table in a database
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+-- creates the database hbtn_0d_usa and the table cities (in the database hbtn_0d_usa) on your MySQL server
+-- creates a database
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- use a database
+USE hbtn_0d_usa;
+-- creates a table
+CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_INCREMENT NOT NULL,
+state_id INT NOT NULL,
+name VARCHAR(256) NOT NULL,
+PRIMARY KEY(id),
+FOREIGN KEY(state_id) REFERENCES states(id));
