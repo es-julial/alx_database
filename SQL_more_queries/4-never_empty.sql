493a917e952fd8fae0cd1c44cf1ed662f43a3a64
Julial ESSEREKE <m_julial@yahoo.co.uk>
2023-08-16 15:28:23 +0200

    Task_4

--- SQL_more_queries/4-never_empty.sql
+++ SQL_more_queries/4-never_empty.sql
@@ -1,34 +1,33 @@
-
--- 3. Always a name
+-- 4. ID can't be null
 -- mandatory
 
--- Write a script that creates the table force_name on your MySQL server.
+-- Write a script that creates the table id_not_null on your MySQL server.
 
---     force_name description:
---         id INT
---         name VARCHAR(256) can’t be null
+--     id_not_null description:
+--         id INT with the default value 1
+--         name VARCHAR(256)
 --     The database name will be passed as an argument of the mysql command
---     If the table force_name already exists, your script should not fail
+--     If the table id_not_null already exists, your script should not fail
 
--- guillaume@ubuntu:~/$ cat 3-force_name.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- guillaume@ubuntu:~/$ cat 4-never_empty.sql | mysql -hlocalhost -uroot -p hbtn_0d_2
 -- Enter password: 
--- guillaume@ubuntu:~/$ echo 'INSERT INTO force_name (id, name) VALUES (89, "Holberton School");' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- guillaume@ubuntu:~/$ echo 'INSERT INTO id_not_null (id, name) VALUES (89, "Holberton School");' | mysql -hlocalhost -uroot -p hbtn_0d_2
 -- Enter password: 
--- guillaume@ubuntu:~/$ echo 'SELECT * FROM force_name;' | mysql -hlocalhost -uroot -p hbtn_0d_2
--- Enter password: 
--- id  name
--- 89  Holberton School
--- guillaume@ubuntu:~/$ echo 'INSERT INTO force_name (id) VALUES (333);' | mysql -hlocalhost -uroot -p hbtn_0d_2
--- Enter password: 
--- ERROR 1364 (HY000) at line 1: Field 'name' doesn't have a default value
--- guillaume@ubuntu:~/$ echo 'SELECT * FROM force_name;' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- guillaume@ubuntu:~/$ echo 'SELECT * FROM id_not_null;' | mysql -hlocalhost -uroot -p hbtn_0d_2
 -- Enter password: 
 -- id  name
 -- 89  Holberton School
--- guillaume@ubuntu:~/$ 
+-- guillaume@ubuntu:~/$ echo 'INSERT INTO id_not_null (name) VALUES ("Holberton");' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- Enter password: 
+-- guillaume@ubuntu:~/$ echo 'SELECT * FROM id_not_null;' | mysql -hlocalhost -uroot -p hbtn_0d_2
+-- Enter password: 
+-- id  name
+-- 89  Holberton School
+-- 1   Holberton
+-- guillaume@ubuntu:~/$
 
 -- //////////////////////////////////////////////////////////////////////////////
 
--- creates the table force_name on your MySQL server
--- creates a table in a database
-CREATE TABLE IF NOT EXISTS force_name (id INT, name VARCHAR(256) NOT NULL);
+-- creates the table id_not_null on your MySQL server
+-- creates a table
+CREATE TABLE IF NOT EXISTS id_not_null (id INT DEFAULT 1, name VARCHAR(256));
